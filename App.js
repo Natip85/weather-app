53f2c1969ec9167ba3f6ec896c4a7fa44454662f
Nati Peretz <nati.peretz1985@gmail.com>
2023-11-09 17:20:49 +0200

    1st

--- App.js
+++ App.js
@@ -1,11 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import Weather from './components/Weather';
+import SearchBar from './components/SearchBar';
+
+const API_KEY = '542b9cf13581d3c84269e1373af1a833'
+
+
 
 export default function App() {
+
+  const [weatherData, setWeatherData] = useState(null)
+  const [loaded, setLoaded] = useState(true)
+
+  async function fetchWeatherData(cityName){
+
+    setLoaded(false)
+
+    const API = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
+
+    try{
+      const response = await fetch(API)
+
+      if(response.status == 200){
+        const data = await response.json()
+        setWeatherData(data)
+
+      }else{
+        setWeatherData(null)
+      }
+      setLoaded(true)
+
+    }catch (error){
+      console.log('OOPS', error)
+    }
+
+  }
+
+  
+  useEffect(()=>{
+  fetchWeatherData('Mumbai')
+  console.log(weatherData);
+  }, [])
+
+
+  if(!loaded){
+    return(
+      <View style={styles.container}>
+        <ActivityIndicator color='gray' size={36} />
+      </View>
+    )
+  }
+
+  else if(weatherData === null) {
+    return (
+      <View style={styles.container}>
+        <SearchBar fetchWeatherData={fetchWeatherData}/>
+        <Text style={styles.primaryText}>City Not Found! Try A Different City</Text>
+      </View>
+    )
+    
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Weather weatherData={weatherData} fetchWeatherData={fetchWeatherData} />
     </View>
   );
 }
@@ -17,4 +76,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+   primaryText: {
+      margin: 20,
+      fontSize: 28
+  }
 });
---
+++ assets/bgImages/index.js
@@ -0,0 +1,6 @@
+const haze = require('../bgImages/haze.jpeg')
+const rainy = require('../bgImages/rainy.webp')
+const snow = require('../bgImages/snow.jpeg')
+const sunny = require('../bgImages/sunny.avif')
+
+export { haze, rainy, snow, sunny }
